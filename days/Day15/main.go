602a4ecc2c5f27f33bf01564481912172634aa76
Blunext <>
2020-12-19 16:15:39 +0100

    Day 1b

--- days/Day15/main.go
+++ days/Day15/main.go
@@ -15,6 +15,15 @@ func Goooo() {
 	input := "2,1,10,11,0,6"
 	//input := "0,3,6"
 
+	lastSpoken := findNumber(input, 2020)
+	fmt.Printf("Part 1: %d\n", lastSpoken)
+
+	lastSpoken = findNumber(input, 30000000)
+	fmt.Printf("Part 2: %d\n", lastSpoken)
+
+}
+
+func findNumber(input string, matchOccurange int) int {
 	memory := make(map[int]*number)
 	lastSpoken := 0
 	i := 1
@@ -23,8 +32,8 @@ func Goooo() {
 		i++
 	}
 
-	for ; i <= 2021; i++ {
-		fmt.Printf("%d: %d\n", i-1, lastSpoken)
+	for ; i <= matchOccurange; i++ {
+		//fmt.Printf("%d: %d\n", i-1, lastSpoken)
 		lastNumber := memory[lastSpoken]
 		if lastNumber.count == 1 {
 			if zero, ok := memory[0]; ok {
@@ -46,9 +55,8 @@ func Goooo() {
 			lastSpoken = newNumber
 			continue
 		}
-
 		memory[newNumber] = &number{pos: i, count: 1}
 		lastSpoken = newNumber
-
 	}
+	return lastSpoken
 }
