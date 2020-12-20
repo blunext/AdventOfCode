36e927b69384a16987c333c78893bc41e53eee30
Blunext <>
2020-12-20 12:43:46 +0100

    Day 16b

--- days/Day16/main.go
+++ days/Day16/main.go
@@ -9,6 +9,8 @@ import (
 type rules struct {
 	name                                       string
 	range1min, range1max, range2min, range2max int
+	rowMatch                                   map[int]bool
+	orderMatch                                 map[int]bool
 }
 
 type ticket struct {
@@ -23,6 +25,7 @@ type tickets struct {
 func Goooo() {
 	fmt.Println("--------- DAY 16 ---------")
 	//lines := tools.ReadFile(("days/Day16/testInput.txt"))
+	//lines := tools.ReadFile(("days/Day16/testInput2.txt"))
 	lines := tools.ReadFile(("days/Day16/input.txt"))
 
 	rulesCollection, lineIndex := getRules(lines)
@@ -44,6 +47,72 @@ func Goooo() {
 
 	fmt.Printf("Part 1: %d\n", errorRate) //25895
 
+	//markRuleRowMatch(rulesCollection, nerbyTickets)
+	//printRulesVsTicketsMatrix(nerbyTickets, rulesCollection)
+	//
+	//
+	//markRuleOrderMatch(rulesCollection, len(nerbyTickets.collection))
+
+	fmt.Printf("Part 2: %d <- to be finished\n", 0) //25895
+}
+
+func markRuleOrderMatch(rulesCollection []rules, ticketNumber int) {
+	for _, rule := range rulesCollection {
+		for ruleIndex, _ := range rulesCollection {
+			ruleExist := true
+			for i := 0; i < ticketNumber; i++ {
+				if !checkRowMatch(rule, i) {
+					ruleExist = false
+					break
+				}
+			}
+			if ruleExist {
+				rule.orderMatch[ruleIndex] = true
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func checkRowMatch(rule rules, i int) bool {
+
+	if _, ok := rule.rowMatch[i]; !ok {
+		return false
+	}
+	return true
+}
+
+func markRuleRowMatch(rulesCollection []rules, nerbyTickets tickets) {
+	for _, rule := range rulesCollection {
+		for i, _ := range nerbyTickets.collection {
+			if checkRuleVsPos(nerbyTickets, i, rule) {
+				rule.rowMatch[i] = true
+			}
+		}
+	}
+}
+
+func printRulesVsTicketsMatrix(nerbyTickets tickets, rulesCollection []rules) {
+	for i := 0; i < len(nerbyTickets.collection); i++ {
+		for _, rule := range rulesCollection {
+			if _, ok := rule.rowMatch[i]; ok {
+				fmt.Print("*")
+			} else {
+				fmt.Print("-")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func checkRuleVsPos(nerbyTickets tickets, pos int, rule rules) bool {
+	for _, field := range nerbyTickets.collection[pos].fields {
+		if !checkOneRule(field, rule) {
+			return false
+		}
+	}
+	return true
 }
 
 func removeInvalidTickets(nerbyTickets *tickets) {
@@ -70,10 +139,6 @@ func checkTicketValidity(nerbyTickets tickets, rulesCollection []rules) int {
 	return errorRate
 }
 
-func remove(slice []int, s int) []int {
-	return append(slice[:s], slice[s+1:]...)
-}
-
 func getNearbyTickets(lineIndex int, lines []string, rulesCollection []rules) tickets {
 	neabyTickets := tickets{}
 	for ; lineIndex < len(lines); lineIndex++ {
@@ -87,9 +152,16 @@ func getNearbyTickets(lineIndex int, lines []string, rulesCollection []rules) ti
 
 func checkRules(rulesCollection []rules, num int) bool {
 	for _, rule := range rulesCollection {
-		if (num >= rule.range1min && num <= rule.range1max) || (num >= rule.range2min && num <= rule.range2max) {
+		if checkOneRule(num, rule) {
 			return true
 		}
+	}
+	return false
+}
+
+func checkOneRule(num int, rule rules) bool {
+	if (num >= rule.range1min && num <= rule.range1max) || (num >= rule.range2min && num <= rule.range2max) {
+		return true
 	}
 	return false
 }
@@ -102,18 +174,18 @@ func getRules(lines []string) ([]rules, int) {
 		if line == "" {
 			break
 		}
-		r := rules{}
+		rule := rules{rowMatch: make(map[int]bool), orderMatch: make(map[int]bool)}
 
 		if strings.Contains(line[:strings.Index(line, ":")], " ") {
 			line = strings.Replace(line, " ", "_", 1)
 		}
 		line = strings.Replace(line, ":", "", 1)
-		_, err := fmt.Sscanf(line, "%s %d-%d or %d-%d", &r.name, &r.range1min, &r.range1max, &r.range2min, &r.range2max)
+		_, err := fmt.Sscanf(line, "%s %d-%d or %d-%d", &rule.name, &rule.range1min, &rule.range1max, &rule.range2min, &rule.range2max)
 		if err != nil {
 			panic(err)
 		}
 		//fmt.Printf("%s: %d-%d or %d-%d\n", rules.range1min, rules.range1max, rules.range2min, rules.range2max)
-		rulesCollection = append(rulesCollection, r)
+		rulesCollection = append(rulesCollection, rule)
 		i++
 	}
 	return rulesCollection, i
